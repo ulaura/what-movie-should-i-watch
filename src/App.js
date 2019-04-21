b8199a4fa0c609757a55dbe6fbc667b36826f272
Laura Unaeze <l.unaeze5@gmail.com>
2019-04-21 15:36:05 -0700

    feat: Got app to make api call with each button click.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movieImdbIdArray: [
+        "tt0133093" /* the matrix */, 
+        "tt3896198" /* guardians of the galaxy, vol. 2 */,
+        "tt0107120" /* hocus pocus */,
+        "tt0115963" /* the craft */
+      ],
+      buttonState: false,
+      data: null,
+    }
+  }
+
+  fetchApiData = () => {
+    let chosenId = this.state.movieImdbIdArray[Math.floor(Math.random() * (this.state.movieImdbIdArray.length))]
+    console.log(chosenId);
+    fetch(`http://www.omdbapi.com/?apikey=4048f070&i=${chosenId}`)
+    .then(response => response.json())
+    .then(data => this.setState({ data }));
+  }
+
+
+  toggleButtonState = () => {
+    this.setState({buttonState: true});
+    this.fetchApiData();
+  }
+
   render() {
+    let { data } = this.state;
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div
+          className="button-div"
+          onClick={this.toggleButtonState}
+        >
+          Find a Movie
+        </div>
+
+        <div className="movie-info-div">
+          {this.state.buttonState && data ? (
+            <p>{data.Title}</p>
+          ):(
+            <p>No movie found....</p>
+          )}
+        </div>
       </div>
     );
   }
